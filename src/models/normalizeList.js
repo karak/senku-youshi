098324ff36b6c5128855068e8461d1ff215b51a7
karak <karak97@gmail.com>
2018-04-15 15:44:53 +0900

    feat: implemenatation normalizeList

--- src/models/normalizeList.js
+++ src/models/normalizeList.js
@@ -1,11 +1,19 @@
+import map from 'lodash/map';
+import split from 'lodash/split';
+import filter from 'lodash/filter';
+import trim from 'lodash/trim';
+
 /**
  * normalize list of works with noise
  *
  * @param {string} value - original value
  * @returns {string} - normalized value
- * 
+ *
  * @todo implementation
  */
 export default function normalizeList(value) {
-  return value;
+  return split(value, /\r?\n/)
+    .map(x => trim(x))
+    .filter((x, i, xs) => x.length > 0 || i === xs.length - 1)
+    .join('\n');
 }
